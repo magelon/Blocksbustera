996d00d18e3be604f0bc2bc60d4649358603b427
magelon <alfardproject@gmail.com>
2019-06-05 20:26:25 -0700

    c

---
+++ Assets/Scripts/cubeMatrix.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeMatrix : MonoBehaviour
+{
+    int x, y, z;
+    public GameObject cube;
+
+    void Start()
+    {
+        x = Random.Range(5, 8);
+        y = Random.Range(5, 8);
+        z = Random.Range(5, 8);
+
+        for (int i = 0; i < x; i++)
+        {
+            for (int j = 0; j < y; j++)
+            {
+                for (int k = 0; k < z; k++)
+                {
+
+                    Instantiate(cube, new Vector3(i, j, k), Quaternion.identity);
+                }
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
