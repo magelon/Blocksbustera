d5e16d8234417a14547663217a54715dd7f3d620
magelon <alfardproject@gmail.com>
2019-06-08 17:02:12 -0700

    c

---
+++ Assets/blockAbyss/Scripts/DropBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropBehavior : MonoBehaviour
+{
+    int count;
+    public cubeMatrix cm;
+    private void OnTriggerEnter(Collider other)
+    {
+        other.GetComponent<Collider>().isTrigger = true;
+        other.GetComponent<Rigidbody>().AddForce(-transform.up*1);
+        GameManager.getInstance().playSfx("drop");
+        count++;
+        Debug.Log(count);
+    }
+
+    private void Update()
+    {
+        if (cm.count == count)
+        {
+            Debug.Log("win");
+            GameData.getInstance().main.gameWin();
+        }
+    }
+
+}
---
+++ Assets/blockAbyss/Scripts/cubeMatrix.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeMatrix : MonoBehaviour
+{
+    int x, y, z;
+    public GameObject cube;
+	public int xStart,zStart;
+    public int count;
+
+    void Start()
+    {
+        x = Random.Range(5, 8);
+        y = Random.Range(5, 8);
+        z = Random.Range(10, 15);
+
+		x = x + xStart;
+		z = z + zStart;
+
+		for (int i = xStart; i < x; i++)
+        {
+            for (int j = 0; j < y; j++)
+            {
+				for (int k = zStart; k < z; k++)
+                {
+                    if (Random.Range(0f, 10f) > 5f)
+                    {
+                        GameObject newCube = Instantiate(cube, new Vector3(i, j, k), Quaternion.identity);
+                        newCube.GetComponent<Renderer>().material.color = new Color(
+                        Random.Range(0f, 1f),
+                        Random.Range(0f, 1f),
+                        Random.Range(0f, 1f)
+                        );
+                        count++;
+                        //Debug.Log(count + "cubes");
+                    }
+                   
+                }
+            }
+        }
+
+    }
+
+    private void Update()
+    {
+        if (Time.timeScale != 1)
+        {
+            Time.timeScale = 1;
+        }
+    }
+
+}
